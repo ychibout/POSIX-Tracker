3275dc31b2b7e356bbbe0c4333313d9beed9785c
Woap <ibis@etu.unistra.fr>
2016-11-26 20:35:27 +0100

    TTL Ok

--- q1-2_tracker.c
+++ q1-2_tracker.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
@@ -6,13 +6,83 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <time.h>
 
-struct elem 
+struct elem
 {
      char hash[65];
      struct in6_addr client;
      short int port;
+     time_t ttl;
+     struct elem* suiv;
 };
+
+int nbelem;
+struct elem *tab;
+
+struct elem* supprimerElement(struct elem* list, time_t valeur)
+{
+struct elem *tmp;
+struct elem *previous;
+ 
+if (list == NULL) // si la liste est NULL on s'arrete tout de suite
+  return (list);
+  
+previous = list;
+if (previous->ttl == valeur) // Verifie la tete de liste, cas particulier
+{
+  list = previous->suiv;
+  free(previous);
+  return (list);
+}
+
+tmp = previous->suiv; // le cas n est gere on se place donc sur le cas n+1
+while(tmp != NULL) // On Mouline est on supprime si on trouve l'element
+{
+  if (tmp->ttl == valeur)
+  {
+    previous->suiv = tmp->suiv;
+    free(tmp);
+    return (list);
+  }
+  previous = tmp; // pour ce souvenir dans la prochaine iteration du precedent
+  tmp = tmp->suiv;
+}
+return list;
+}
+
+
+void* doSomeThing(void *arg)
+{
+    time_t seconds;
+    int i=0;
+    //struct elem *tab = (struct elem *) arg;
+    struct elem *temp;
+    temp = tab;
+
+    while (1)
+   {
+     temp = tab;
+     sleep(1);
+
+     seconds = time(NULL);
+
+      while (temp != NULL)
+     {
+
+        if(temp->ttl == seconds)
+        {
+          printf("Suppression element : temp->hash = %s \n",temp->hash);
+          tab = supprimerElement(tab,temp->ttl);
+          nbelem--;
+        }
+        temp = temp->suiv;
+      }
+   }
+
+
+    return NULL;
+}
 
 int main(int argc, char **argv)
 {
@@ -49,18 +119,18 @@ int main(int argc, char **argv)
 
 
 
-     /*PARTIE 2 : socket de reception de données*/
+     /*PARTIE 2 : socket de reception de donn�es*/
 
      int sockfd; // descripteur
      socklen_t addrlen; // socket
      struct sockaddr_in6 my_addr; // adresse ipv6
      struct sockaddr_in6 client;	// adresse ipv6 client
 
-     int sockfd2;   //socket de la réponse
+     int sockfd2;   //socket de la r�ponse
      struct sockaddr_in6 dest;     //adresse socket
      socklen_t addrlen2; //longueur de l'adresse
 
-     // socket à créer
+     // socket du cr�er
      if((sockfd = socket(AF_INET6,SOCK_DGRAM,IPPROTO_UDP)) == -1)
      {
           perror("socket fail\n");
@@ -109,10 +179,21 @@ int main(int argc, char **argv)
 
 
      //tableau de réception
-     struct elem *tab = (struct elem*)malloc(sizeof(struct elem));
-     int fait = 0;
-     int nbelem = 1;
+     int test = 0;
+     nbelem = 0;
+     struct elem* temp;
 
+     // tracker
+     
+     time_t seconds;
+     pthread_t t;
+    int err;
+
+    err = pthread_create(&t, NULL, &doSomeThing, (void*)&tab[0]);
+        if (err != 0)
+            printf("\ncan't create thread :[%s]", strerror(err));
+        else
+            printf("\n Thread created successfully\n");
 
      // reception de la chaine de caracteres
      while (1)
@@ -137,30 +218,42 @@ int main(int argc, char **argv)
 
           if (msg_type == 110)
           {
-
-               if (fait == 0)
+               if (nbelem == 0)
                {
-                    strcpy(tab[0].hash, hash);
-                    tab[0].client = client_addr;
-                    tab[0].port = client_port;
-                    fait = 1;
+                    nbelem++;
+                    tab = (struct elem*)malloc(sizeof(struct elem));
+                    strcpy(tab->hash, hash);
+                    tab->client = client_addr;
+                    tab->port = client_port;
+                    tab->ttl = time(NULL)+10;
+                    tab->suiv=NULL;
+                    test = 1;
                     printf("\n\nNouvelle entrée : \n");
-                    printf("Hash : %s\n", tab[0].hash);
-                    printf("Adresse client : %s\n", inet_ntop(AF_INET6, &tab[0].client, client_tst, sizeof(tab[0].client)));
-                    printf("Port : %d\n", tab[0].port);
+                    printf("Hash : %s\n", tab->hash);
+                    printf("Adresse client : %s\n", inet_ntop(AF_INET6, &tab->client, client_tst, sizeof(tab->client)));
+                    printf("Port : %d\n", tab->port);
                     printf("nb éléments dans le tracker : %d\n\n", nbelem);
                }
                else
                {
                     nbelem++;
-                    tab = (struct elem*)realloc(tab, nbelem*sizeof(struct elem));
-                    strcpy(tab[nbelem-1].hash, hash);
-                    tab[nbelem-1].client = client_addr;
-                    tab[nbelem-1].port = client_port;
+                    temp = tab;
+                    while (temp->suiv != NULL)
+                    {
+                         temp = temp->suiv;
+                    }
+                    struct elem* new =  (struct elem*)malloc(sizeof(struct elem));
+                    strcpy(new->hash, hash);
+                    new->client = client_addr;
+                    new->port = client_port;
+                    new->ttl = time(NULL)+10;
+                    new->suiv=NULL;
+                    
+                    temp->suiv = new;
                     printf("\n\nNouvelle entrée : \n");
-                    printf("Hash : %s\n", tab[nbelem-1].hash);
-                    printf("Adresse client : %s\n", inet_ntop(AF_INET6, &tab[nbelem-1].client, client_tst, sizeof(tab[nbelem-1].client)));
-                    printf("Port : %d\n", tab[nbelem-1].port);
+                    printf("Hash : %s\n", new->hash);
+                    printf("Adresse client : %s\n", inet_ntop(AF_INET6, &new->client, client_tst, sizeof(new->client)));
+                    printf("Port : %d\n", new->port);
                     printf("nb éléments dans le tracker : %d\n\n", nbelem);
                }
 
@@ -170,11 +263,11 @@ int main(int argc, char **argv)
                memcpy(rep+sizeof(msg_type), &msg_len, sizeof(msg_len)); //taille message
                memcpy(rep+sizeof(msg_type)+sizeof(msg_len), &hash_type, sizeof(hash_type)); //type hash
                memcpy(rep+sizeof(msg_type)+sizeof(msg_len)+sizeof(hash_type), &hash_len, sizeof(hash_len));   //taille hash
-               memcpy(rep+sizeof(msg_type)+sizeof(msg_len)+sizeof(hash_type)+sizeof(hash_len), tab[nbelem-1].hash, sizeof(hash)); //hash
+               memcpy(rep+sizeof(msg_type)+sizeof(msg_len)+sizeof(hash_type)+sizeof(hash_len), hash, sizeof(hash)); //hash
                memcpy(rep+sizeof(msg_type)+sizeof(msg_len)+sizeof(hash_type)+sizeof(hash_len)+sizeof(hash), &client_type, sizeof(client_type));   //type client
                memcpy(rep+sizeof(msg_type)+sizeof(msg_len)+sizeof(hash_type)+sizeof(hash_len)+sizeof(hash)+sizeof(client_type), &client_len, sizeof(client_len));   //taille client
-               memcpy(rep+sizeof(msg_type)+sizeof(msg_len)+sizeof(hash_type)+sizeof(hash_len)+sizeof(hash)+sizeof(client_type)+sizeof(client_len), &tab[nbelem-1].port, sizeof(client_port)); //port client
-               memcpy(rep+sizeof(msg_type)+sizeof(msg_len)+sizeof(hash_type)+sizeof(hash_len)+sizeof(hash)+sizeof(client_type)+sizeof(client_len)+sizeof(client_port), &tab[nbelem-1].client, sizeof(client_addr));   //adresse client
+               memcpy(rep+sizeof(msg_type)+sizeof(msg_len)+sizeof(hash_type)+sizeof(hash_len)+sizeof(hash)+sizeof(client_type)+sizeof(client_len), &client_port, sizeof(client_port)); //port client
+               memcpy(rep+sizeof(msg_type)+sizeof(msg_len)+sizeof(hash_type)+sizeof(hash_len)+sizeof(hash)+sizeof(client_type)+sizeof(client_len)+sizeof(client_port), &client_addr, sizeof(client_addr));   //adresse client
 
                if((sockfd2 = socket(AF_INET6,SOCK_DGRAM,IPPROTO_UDP)) == -1)
                {
@@ -183,8 +276,8 @@ int main(int argc, char **argv)
                }
 
                dest.sin6_family = AF_INET6;
-               dest.sin6_addr   = tab[nbelem-1].client;
-               dest.sin6_port   = htons(tab[nbelem-1].port);
+               dest.sin6_addr   = client_addr;
+               dest.sin6_port   = htons(client_port);
                addrlen2         = sizeof(struct sockaddr_in6);
 
                if (sendto(sockfd2, rep, msg_len+1+2, 0, (struct sockaddr *) &dest, addrlen2) == -1)
@@ -195,20 +288,20 @@ int main(int argc, char **argv)
                }
                printf("ACK PUT envoyé \n");
           }
-		  
+
 
           if (msg_type == 112)
           {
-			   char rmsg_type;
-      			short int rmsg_len;
-      			char rhash_type;
-      			short int rhash_len;
-      			char* rhash[65];
-      			char rclient_type;
-      			short int rclient_len;
-      			short int rclient_port;
-      			char* rclient = (char*)malloc(50);
-      			struct in6_addr rclient_addr;
+               char rmsg_type;
+               short int rmsg_len;
+               char rhash_type;
+               short int rhash_len;
+               char* rhash[65];
+               char rclient_type;
+               short int rclient_len;
+               short int rclient_port;
+               char* rclient = (char*)malloc(50);
+               struct in6_addr rclient_addr;
 
 
                short int msg_len2 = 68;
@@ -221,50 +314,60 @@ int main(int argc, char **argv)
                memcpy(rep+sizeof(msg_type)+sizeof(msg_len)+sizeof(hash_type), &hash_len, sizeof(hash_len)); // taille hash
                memcpy(rep+sizeof(msg_type)+sizeof(msg_len)+sizeof(hash_type)+sizeof(hash_len), hash, sizeof(hash)); // hash
 
-               
-               int i;
-               for (i = 0; i < nbelem; i++)
+
+               temp = tab;
+               while (temp != NULL)
                {
-                    if (strcmp(tab[i].hash, hash) == 0)
+                    if (strcmp(temp->hash, hash) == 0)
                     {
                          nbclients++;
                          rep = realloc(rep, 71+nbclients*21);
                          memcpy(rep+71+(nbclients-1)*21, &client_type, sizeof(client_type));
                          memcpy(rep+71+(nbclients-1)*21+sizeof(client_type), &client_len, sizeof(client_len));
-                         memcpy(rep+71+(nbclients-1)*21+sizeof(client_type)+sizeof(client_len), &tab[i].port, sizeof(client_port));
-                         memcpy(rep+71+(nbclients-1)*21+sizeof(client_type)+sizeof(client_len)+sizeof(client_port), &tab[i].client, sizeof(client_addr));
+                         memcpy(rep+71+(nbclients-1)*21+sizeof(client_type)+sizeof(client_len), &temp->port, sizeof(client_port));
+                         memcpy(rep+71+(nbclients-1)*21+sizeof(client_type)+sizeof(client_len)+sizeof(client_port), &temp->client, sizeof(client_addr));
 
-                  memcpy(&rclient_type, rep+71+(nbclients-1)*21, sizeof(rclient_type));   
-       				    memcpy(&rclient_len,  rep+71+(nbclients-1)*21+sizeof(client_type), sizeof(rclient_len));   
-                  memcpy(&rclient_port, rep+71+(nbclients-1)*21+sizeof(client_type)+sizeof(client_len), sizeof(rclient_port));
-                  memcpy(&rclient_addr, rep+71+(nbclients-1)*21+sizeof(client_type)+sizeof(client_len)+sizeof(client_port), sizeof(rclient_addr));
+                         memcpy(&rclient_type, rep+71+(nbclients-1)*21, sizeof(rclient_type));
+                         memcpy(&rclient_len,  rep+71+(nbclients-1)*21+sizeof(client_type), sizeof(rclient_len));
+                         memcpy(&rclient_port, rep+71+(nbclients-1)*21+sizeof(client_type)+sizeof(client_len), sizeof(rclient_port));
+                         memcpy(&rclient_addr, rep+71+(nbclients-1)*21+sizeof(client_type)+sizeof(client_len)+sizeof(client_port), sizeof(rclient_addr));
 
-                /*printf("client_type : %u\n", rclient_type);
-                  printf("client_len : %d\n", rclient_len);
-                  printf("client_port : %d\n", rclient_port);
-                  printf("client : %s\n", inet_ntop(AF_INET6, &rclient_addr, rclient, sizeof(rclient_addr)));*/
+                         /*printf("client_type : %u\n", rclient_type);
+                         printf("client_len : %d\n", rclient_len);
+                         printf("client_port : %d\n", rclient_port);
+                         printf("client : %s\n", inet_ntop(AF_INET6, &rclient_addr, rclient, sizeof(rclient_addr)));
+                         */
                     }
-                    
-                    
+
+                  temp = temp->suiv;
                }
-               
+
                if ( nbclients != 0)
+               {
+                    nbelem++;
+                    temp = tab;
+                   while (temp->suiv != NULL)
                     {
-                      nbelem++;
-                    tab = (struct elem*)realloc(tab, nbelem*sizeof(struct elem));
-                    strcpy(tab[nbelem-1].hash, hash);
-                    tab[nbelem-1].client = client_addr;
-                    tab[nbelem-1].port = client_port;
-                    printf("\n\nNouvelle entrée : \n");
-                    printf("Hash : %s\n", tab[nbelem-1].hash);
-                    printf("Adresse client : %s\n", inet_ntop(AF_INET6, &tab[nbelem-1].client, client_tst, sizeof(tab[nbelem-1].client)));
-                    printf("Port : %d\n", tab[nbelem-1].port);
-                    printf("nb éléments dans le tracker : %d\n\n", nbelem);
+                         temp = temp->suiv;
                     }
-              // printf("Nb clients : %d \n",nbclients);
+                    struct elem* new =  (struct elem*)malloc(sizeof(struct elem));
+                    strcpy(new->hash, hash);
+                    new->client = client_addr;
+                    new->port = client_port;
+                    new->ttl = time(NULL)+10;
+                    new->suiv=NULL;
+                    
+                    temp->suiv = new;
+                    printf("\n\nNouvelle entrée: \n");
+                    printf("Hash : %s\n", new->hash);
+                    printf("Adresse client : %s\n", inet_ntop(AF_INET6, &new->client, client_tst, sizeof(new->client)));
+                    printf("Port : %d\n", new->port);
+                    printf("nb éléments dans le tracker : %d\n\n", nbelem);
+               }
+               // printf("Nb clients : %d \n",nbclients);
                msg_len2+=21*nbclients;
-              // printf("msg_len %u \n",msg_len2);
-               memcpy(rep+sizeof(msg_type), &msg_len2, sizeof(msg_len2)); 
+               // printf("msg_len %u \n",msg_len2);
+               memcpy(rep+sizeof(msg_type), &msg_len2, sizeof(msg_len2));
 
                if((sockfd2 = socket(AF_INET6,SOCK_DGRAM,IPPROTO_UDP)) == -1)
                {
